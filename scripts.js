8e532e069134283ea9185cd1cda91026c2a6fc35
babu <babu@debABU>
2020-08-07 19:04:39 -0400

    working relatively well now

--- scripts.js
+++ scripts.js
@@ -27,44 +27,60 @@ function displayCovidData() {
     let reportDateArray = []
     let testsCompleted = []
     let confirmedPositiveCovidPercentageOfTest = []
+    let covidInHospital = [];
+    let confirmedPositiveDoubling = [];
+    let confirmedDeathsDoubling = [];
 
     // function renderData(data) {
     //     return `<h1> ${data[0]["Reported Date"]} </h1>`;
     // }
 
     fetchCovidData().then((data) => {
-        // console.log(data);
-        
+        console.log(data);
+
         data.map((entry) => {
             const _testCompleted = entry["Total tests completed in the last day"];
             const confirmedPositive = entry["Confirmed Positive"];
-            
-            
-              const oneDaysPercentagePositiveResults = _testCompleted === 0 || _testCompleted == null  ? null : roundThis((confirmedPositive / _testCompleted) * 100);
-            //const oneDaysPercentagePositiveResults = (confirmedPositive / _testCompleted) * 100;
-             confirmedPositiveCovidPercentageOfTest.push(oneDaysPercentagePositiveResults);
-            testsCompleted.push(_testCompleted/1000)
-            reportDateArray.push(entry["Reported Date"])
-            
+            const hospitalCovid = entry["Number of patients hospitalized with COVID-19"];
+            const _confirmedPositiveDoubling = entry["Number of Days for Cumulative Confirmed Positive to Double"];
+            const _confirmedDeathsDoubling = entry["Number of Days for Deaths to Double"];
+
+
+            const oneDaysPercentagePositiveResults = _testCompleted === 0 || _testCompleted == null ? null : roundThis((confirmedPositive / _testCompleted) * 100);
+            confirmedPositiveCovidPercentageOfTest.push(oneDaysPercentagePositiveResults);
+            testsCompleted.push(_testCompleted / 1000);
+            covidInHospital.push(hospitalCovid);
+            reportDateArray.push(entry["Reported Date"]);
+            confirmedPositiveDoubling.push(_confirmedPositiveDoubling);
+            confirmedDeathsDoubling.push(_confirmedDeathsDoubling);
+
         });
-        console.log(confirmedPositiveCovidPercentageOfTest)
-        reportDateArray = reportDateArray.slice(Math.max(reportDateArray.length - 55,0))
-    
-        testsCompleted = testsCompleted.slice(Math.max(testsCompleted.length - 55,0))
-        confirmedPositiveCovidPercentageOfTest = confirmedPositiveCovidPercentageOfTest.slice(Math.max(confirmedPositiveCovidPercentageOfTest.length - 55,0))
+        const last_x_days = 55;
+        reportDateArray = getLastEntries(reportDateArray, last_x_days);
+        testsCompleted = getLastEntries(testsCompleted, last_x_days);
+        confirmedPositiveCovidPercentageOfTest = getLastEntries(confirmedPositiveCovidPercentageOfTest, last_x_days);
+        covidInHospital = getLastEntries(covidInHospital, last_x_days);
+        confirmedPositiveDoubling = getLastEntries(confirmedPositiveDoubling, last_x_days);
+        confirmedDeathsDoubling = getLastEntries(confirmedDeathsDoubling, last_x_days);
 
-        CovidChart("chart", reportDateArray, testsCompleted, "# of Tests Completed (in '000's)"); 
-        CovidChart("chart2", reportDateArray, confirmedPositiveCovidPercentageOfTest, "Positive Covid Test Results (in %'s)"); 
 
-        CovidChart("chart3", reportDateArray, testsCompleted, "# of Tests Completed (in '000's)"); 
-        CovidChart("chart4", reportDateArray, confirmedPositiveCovidPercentageOfTest, "Positive Covid Test Results (in %'s)"); 
+        CovidChart("chart", reportDateArray, testsCompleted, "# of Tests Completed (in '000's)");
+        CovidChart("chart2", reportDateArray, confirmedPositiveCovidPercentageOfTest, "Positive Covid Test Results (in %'s)");
+
+        CovidChart("chart3", reportDateArray, covidInHospital, "# of Patients in Hospital");
+        CovidChart("chart4", reportDateArray, confirmedPositiveDoubling, "# of Days for Positive Cases To Double"); 
+        CovidChart("chart5", reportDateArray, confirmedPositiveDoubling, "# of Days for Deaths To Double"); 
+
         // const html = renderData(data);
         // mainSection.insertAdjacentHTML('beforeend', html);
 
     }
     );
 }
-
+function getLastEntries(thisArray, x) {
+    console.log("mother father");
+    return thisArray.slice(Math.max(thisArray.length - x, 0));
+}
 
 
 function roundThis(int) {
