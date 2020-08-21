f51c7242a19ebb86e2211e14c1a0321991713aaf
babu <babu@debABU>
2020-08-20 21:59:57 -0400

    improved code formatting via prettier

--- src/apex-charting.ts
+++ src/apex-charting.ts
@@ -1,125 +1,144 @@
-import {roundThis} from './utils'
-import ApexCharts = require('apexcharts');
-import { removeMonthsTooCloseToEachOther, getFirstOfTheMonthEntriesWhileNullingOutOtherDates } from './chart-utils'
+import { roundThis } from "./utils";
+import ApexCharts = require("apexcharts");
+import {
+  removeMonthsTooCloseToEachOther,
+  getFirstOfTheMonthEntriesWhileNullingOutOtherDates,
+} from "./chart-utils";
 
-export default function ChartWithApex(htmlTagId: string, x_labels: Array<any>, y_data: Array<any>, chart_title: string, chart_unit?: string): void {
+export default function ChartWithApex(
+  htmlTagId: string,
+  x_labels: Array<any>,
+  y_data: Array<any>,
+  chart_title: string,
+  chart_unit?: string
+): void {
+  let mod_x_labels = getFirstOfTheMonthEntriesWhileNullingOutOtherDates(
+    x_labels
+  );
+  mod_x_labels = removeMonthsTooCloseToEachOther(25, mod_x_labels);
+  const axes_colour: string = "#BF880D";
+  // console.log("in Apex " + htmlTagId);
+  const options: object = {
+    chart: {
+      type: "line",
+      toolbar: {
+        show: false,
+      },
+    },
+    grid: {
+      show: false,
+    },
+    stroke: {
+      show: true,
+      curve: "smooth",
+      lineCap: "butt",
+      // colors: undefined,
+      width: 2,
+      dashArray: 0,
+    },
+    title: {
+      text: chart_title,
+      align: "left",
+      margin: 10,
+      offsetX: 0,
+      offsetY: 0,
+      floating: false,
+      style: {
+        fontSize: "15px",
 
-    let mod_x_labels = getFirstOfTheMonthEntriesWhileNullingOutOtherDates(x_labels);
-    mod_x_labels = removeMonthsTooCloseToEachOther(25, mod_x_labels);
-    const axes_colour: string = "#BF880D";
-    // console.log("in Apex " + htmlTagId);
-    const options: object = {
-        chart: {
-            type: 'line',
-            toolbar: {
-                show: false,
-            }
+        fontFamily: "Roboto",
+        color: "#FFFFFF",
+      },
+    },
+    colors: ["#778DA9"],
+    series: [
+      {
+        name: chart_title,
+        data: y_data,
+      },
+    ],
+    xaxis: {
+      categories: mod_x_labels,
+      labels: {
+        maxHeight: 120,
+        style: {
+          colors: axes_colour,
+          fontSize: "10px", // font for mobile
+          fontFamily: "Roboto",
+          // cssClass: 'apex-font',
         },
-        grid: {
-            show: false,
+      },
+      axisBorder: {
+        show: false,
+        color: "#BF880D",
+        height: 1,
+        width: "100%",
+        offsetX: 0,
+        offsetY: 0,
+      },
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: axes_colour,
+          fontFamily: "Roboto",
         },
-        stroke: {
-            show: true,
-            curve: 'smooth',
-            lineCap: 'butt',
-            // colors: undefined,
-            width: 2,
-            dashArray: 0,
-        }
-        ,
-        title: {
-            text: chart_title,
-            align: 'left',
-            margin: 10,
-            offsetX: 0,
-            offsetY: 0,
-            floating: false,
-            style: {
-                fontSize: '15px',
-
-                fontFamily: 'Roboto',
-                color: '#FFFFFF'
-            },
+        formatter: (value: any) => {
+          return roundThis(value, 1);
         },
-        colors: ['#778DA9'],
-        series: [{
-            name: chart_title,
-            data: y_data
-        }],
-        xaxis: {
-            categories: mod_x_labels,
-            labels: {
-                maxHeight: 120,
-                style: {
-                    colors: axes_colour,
-                    fontSize:  '10px', // font for mobile
-                    fontFamily: 'Roboto',
-                    // cssClass: 'apex-font',
-
-                },
-            }
-            ,axisBorder: {
-                show: false,
-                color: '#BF880D',
-                height: 1,
-                width: '100%',
-                offsetX: 0,
-                offsetY: 0
-            },
+      },
+    },
+    tooltip: {
+      enabled: true,
+      x: {
+        formatter: function (value: any) {
+          return x_labels[value - 1];
         },
-        yaxis: {
-            labels: {
-                style: {
-                    colors: axes_colour,
-                    fontFamily: 'Roboto'
-                },
-                formatter: (value : any) => { return roundThis(value, 1) }
-            }
+      },
+    },
+    markers: {
+      size: 0.1,
+      colors: undefined,
+      strokeColors: "#fff",
+      strokeWidth: 2,
+      strokeOpacity: 0.9,
+      strokeDashArray: 0,
+      fillOpacity: 1,
+      discrete: [
+        {
+          seriesIndex: 0,
+          dataPointIndex: y_data.length - 1,
+          fillColor: "#e3e3e3",
+          strokeColor: "#fff",
+          size: 3,
         },
-        tooltip: {
-            enabled: true,
-            x: {
-                formatter: function (value: any) {
-                    return x_labels[value - 1]
-                }
-            },
-        },
-        markers: {
-            size: 0.1,
-            colors: undefined,
-            strokeColors: '#fff',
-            strokeWidth: 2,
-            strokeOpacity: 0.9,
-            strokeDashArray: 0,
-            fillOpacity: 1,
-            discrete: [{
-                seriesIndex: 0,
-                dataPointIndex: y_data.length - 1,
-                fillColor: '#e3e3e3',
-                strokeColor: '#fff',
-                size: 3
-              }],
-            shape: "circle",
-            radius: 2
-        }
-        
+      ],
+      shape: "circle",
+      radius: 2,
+    },
+  };
+  const thisTag: HTMLDivElement = document.querySelector(htmlTagId);
+  const figure: HTMLElement = thisTag.getElementsByTagName("figure")[0];
 
+  setBackgroundOfFigure(thisTag, y_data, chart_unit);
 
-    };
-    const thisTag : HTMLDivElement = document.querySelector(htmlTagId)
-    const figure : HTMLElement =  thisTag.getElementsByTagName("figure")[0];
+  const chart: ApexCharts = new ApexCharts(figure, options);
 
-    setBackgroundOfFigure(thisTag, y_data, chart_unit);
-
-    const chart: ApexCharts = new ApexCharts(figure, options);
-
-    chart.render();
+  chart.render();
 }
 
-function setBackgroundOfFigure(thisTag: HTMLDivElement, y_data: any[], chart_unit: string) {
-    const figureBackground: HTMLElement = thisTag.getElementsByTagName("figcaption")[0];
-    const lastItemOfY: number = <number>(y_data[y_data.length - 1]);
-    const suffixContent: string | null = chart_unit ? chart_unit : "";
-    const content = document.createTextNode(String(roundThis(lastItemOfY, 1)) + suffixContent);
-    figureBackground.appendChild(content);
+function setBackgroundOfFigure(
+  thisTag: HTMLDivElement,
+  y_data: any[],
+  chart_unit: string
+) {
+  const figureBackground: HTMLElement = thisTag.getElementsByTagName(
+    "figcaption"
+  )[0];
+  const lastItemOfY: number = <number>y_data[y_data.length - 1];
+  const suffixContent: string | null = chart_unit ? chart_unit : "";
+  const content = document.createTextNode(
+    String(roundThis(lastItemOfY, 1)) + suffixContent
+  );
+  figureBackground.appendChild(content);
 }
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,111 +1,143 @@
 // import CovidChart from './charting'
-import ChartWithApex from './apex-charting'
-import {getLastEntries, roundThis} from './utils'
+import ChartWithApex from "./apex-charting";
+import { getLastEntries, roundThis } from "./utils";
 
 function fetchCovidData() {
+  const endpoint: string = window.encodeURI(
+    `https://nl81s06nk2.execute-api.ca-central-1.amazonaws.com/Prod/covid/`
+  );
 
-    const endpoint: string = window.encodeURI(`https://nl81s06nk2.execute-api.ca-central-1.amazonaws.com/Prod/covid/`);
-
-    return fetch(endpoint)
-        .then(res =>
-            res.json()
-        )
-        .then((data) => {
-            if (!data[0]) {
-
-                console.log("WTF NOTHONG THERE IDIOT!!!")
-            }
-            // console.log("okay returning some data i think")
-            return data
-        }
-
-        )
+  return fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data[0]) {
+        console.log("WTF NOTHONG THERE IDIOT!!!");
+      }
+      // console.log("okay returning some data i think")
+      return data;
+    });
 }
 
-const mainSection : Element = document.querySelector('.coral');
+const mainSection: Element = document.querySelector(".coral");
 
+function displayCovidData(): void {
+  let reportDateArray: Array<string> = [];
+  let testsCompleted: Array<number> = [];
+  let confirmedPositiveCovidPercentageOfTest: Array<number> = [];
+  let confirmedPositive: Array<number> = [];
+  let covidInHospital: Array<bigint> = [];
+  let confirmedPositiveDoubling: Array<bigint> = [];
+  let confirmedDeathsDoubling: Array<bigint> = [];
 
-function displayCovidData() : void {
-    let reportDateArray : Array<string> = []
-    let testsCompleted : Array<number> = []
-    let confirmedPositiveCovidPercentageOfTest : Array<number> = []
-    let confirmedPositive : Array<number> = [];
-    let covidInHospital : Array<bigint> = [];
-    let confirmedPositiveDoubling : Array<bigint> = [];
-    let confirmedDeathsDoubling : Array<bigint> = [];
+  fetchCovidData().then((data) => {
+    // console.log(data);
 
+    data.map((entry: any) => {
+      const _testCompleted: number | null =
+        entry["Total tests completed in the last day"];
+      const _confirmedPositive: number | null = entry["Confirmed Positive"];
+      const _dailyConfirmedPositive: number | null =
+        entry["Daily Confirmed Positive"];
+      const hospitalCovid: bigint | null =
+        entry["Number of patients hospitalized with COVID-19"];
+      const _confirmedPositiveDoubling: bigint | null =
+        entry["Number of Days for Cumulative Confirmed Positive to Double"];
+      const _confirmedDeathsDoubling: bigint | null =
+        entry["Number of Days for Deaths to Double"];
 
-    fetchCovidData().then((data) => {
-        // console.log(data);
-
-        data.map((entry : any) => {
-            const _testCompleted : number | null = entry["Total tests completed in the last day"];
-            const _confirmedPositive : number | null = entry["Confirmed Positive"];
-            const _dailyConfirmedPositive : number | null = entry["Daily Confirmed Positive"];
-            const hospitalCovid : bigint | null = entry["Number of patients hospitalized with COVID-19"];
-            const _confirmedPositiveDoubling : bigint | null = entry["Number of Days for Cumulative Confirmed Positive to Double"];
-            const _confirmedDeathsDoubling : bigint | null = entry["Number of Days for Deaths to Double"];
-
-
-            const oneDaysPercentagePositiveResults : any = _testCompleted === 0 || _testCompleted == null ? null : roundThis((_dailyConfirmedPositive / _testCompleted) * 100, 2);
-            confirmedPositiveCovidPercentageOfTest.push(oneDaysPercentagePositiveResults);
-            confirmedPositive.push(_confirmedPositive);
-            testsCompleted.push(_testCompleted / 1000);
-            covidInHospital.push(hospitalCovid);
-            reportDateArray.push(entry["Reported Date"]);
-            confirmedPositiveDoubling.push(_confirmedPositiveDoubling);
-            confirmedDeathsDoubling.push(_confirmedDeathsDoubling);
-
-        });
-        const last_x_days = 55;
-        reportDateArray = getLastEntries(reportDateArray, last_x_days);
-        testsCompleted = getLastEntries(testsCompleted, last_x_days);
-        confirmedPositive = getLastEntries(confirmedPositive, last_x_days);
-        confirmedPositiveCovidPercentageOfTest = getLastEntries(confirmedPositiveCovidPercentageOfTest, last_x_days);
-        covidInHospital = getLastEntries(covidInHospital, last_x_days);
-        confirmedPositiveDoubling = getLastEntries(confirmedPositiveDoubling, last_x_days);
-        confirmedDeathsDoubling = getLastEntries(confirmedDeathsDoubling, last_x_days);
-
-
-        document.querySelector("#loading").classList.add("hidden");
-
-        setTemplate("#ontario-chart");
-        setUpdatedDate(reportDateArray);
-
-        ChartWithApex("#chart6", reportDateArray, testsCompleted, "Tested", "K");
-        ChartWithApex("#chart", reportDateArray, confirmedPositive, "Confirmed Positive");
-        ChartWithApex("#chart2", reportDateArray, confirmedPositiveCovidPercentageOfTest, "Percent Positivity", "%");
-
-        ChartWithApex("#chart3", reportDateArray, covidInHospital, "Hospitalizations");
-        ChartWithApex("#chart4", reportDateArray, confirmedPositiveDoubling, "Days To Double Infections"); 
-        ChartWithApex("#chart5", reportDateArray, confirmedPositiveDoubling, "Days To Double Deaths"); 
- 
-
-    }
+      const oneDaysPercentagePositiveResults: any =
+        _testCompleted === 0 || _testCompleted == null
+          ? null
+          : roundThis((_dailyConfirmedPositive / _testCompleted) * 100, 2);
+      confirmedPositiveCovidPercentageOfTest.push(
+        oneDaysPercentagePositiveResults
+      );
+      confirmedPositive.push(_confirmedPositive);
+      testsCompleted.push(_testCompleted / 1000);
+      covidInHospital.push(hospitalCovid);
+      reportDateArray.push(entry["Reported Date"]);
+      confirmedPositiveDoubling.push(_confirmedPositiveDoubling);
+      confirmedDeathsDoubling.push(_confirmedDeathsDoubling);
+    });
+    const last_x_days = 55;
+    reportDateArray = getLastEntries(reportDateArray, last_x_days);
+    testsCompleted = getLastEntries(testsCompleted, last_x_days);
+    confirmedPositive = getLastEntries(confirmedPositive, last_x_days);
+    confirmedPositiveCovidPercentageOfTest = getLastEntries(
+      confirmedPositiveCovidPercentageOfTest,
+      last_x_days
     );
+    covidInHospital = getLastEntries(covidInHospital, last_x_days);
+    confirmedPositiveDoubling = getLastEntries(
+      confirmedPositiveDoubling,
+      last_x_days
+    );
+    confirmedDeathsDoubling = getLastEntries(
+      confirmedDeathsDoubling,
+      last_x_days
+    );
+
+    document.querySelector("#loading").classList.add("hidden");
+
+    setTemplate("#ontario-chart");
+    setUpdatedDate(reportDateArray);
+
+    ChartWithApex("#chart6", reportDateArray, testsCompleted, "Tested", "K");
+    ChartWithApex(
+      "#chart",
+      reportDateArray,
+      confirmedPositive,
+      "Confirmed Positive"
+    );
+    ChartWithApex(
+      "#chart2",
+      reportDateArray,
+      confirmedPositiveCovidPercentageOfTest,
+      "Percent Positivity",
+      "%"
+    );
+
+    ChartWithApex(
+      "#chart3",
+      reportDateArray,
+      covidInHospital,
+      "Hospitalizations"
+    );
+    ChartWithApex(
+      "#chart4",
+      reportDateArray,
+      confirmedPositiveDoubling,
+      "Days To Double Infections"
+    );
+    ChartWithApex(
+      "#chart5",
+      reportDateArray,
+      confirmedPositiveDoubling,
+      "Days To Double Deaths"
+    );
+  });
 }
 
 function setUpdatedDate(reportDateArray: string[]) {
-    const dateElement: HTMLElement = document.querySelector("#update-date");
-    const lastDate: string = reportDateArray[reportDateArray.length - 1];
-    const dateContent = document.createTextNode("Last Updated: " + String(lastDate));
-    dateElement.appendChild(dateContent);
+  const dateElement: HTMLElement = document.querySelector("#update-date");
+  const lastDate: string = reportDateArray[reportDateArray.length - 1];
+  const dateContent = document.createTextNode(
+    "Last Updated: " + String(lastDate)
+  );
+  dateElement.appendChild(dateContent);
 }
 
-function setTemplate(templateId : string) {
-    if ('content' in document.createElement('template')) {
-        // browser supports templating
-        
-        const template : HTMLTemplateElement = document.querySelector(templateId);
-        const cloned_template = template.content.cloneNode(true);
-        const body : HTMLElement = document.getElementsByTagName('main')[0];
-        body.appendChild(cloned_template);
-        
-    }
-    else {
-        console.log("Get a better browser ffs!");
-    }
-}
+function setTemplate(templateId: string) {
+  if ("content" in document.createElement("template")) {
+    // browser supports templating
 
+    const template: HTMLTemplateElement = document.querySelector(templateId);
+    const cloned_template = template.content.cloneNode(true);
+    const body: HTMLElement = document.getElementsByTagName("main")[0];
+    body.appendChild(cloned_template);
+  } else {
+    console.log("Get a better browser ffs!");
+  }
+}
 
 displayCovidData();
